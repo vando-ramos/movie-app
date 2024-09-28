2e3b750def8529d8b66b658cdf38f18f04dc8ce9
vando <vandorramos@gmail.com>
2024-09-28 14:26:09 -0300

    Creating a form for register new directors, config registers controller and model, and show them in home page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,14 +1,23 @@
 class DirectorsController < ApplicationController
+  before_action :set_director, only: %i[show edit update destroy]
+
   def show
 
   end
 
   def new
-
+    @director = Director.new
   end
 
   def create
+    @director = Director.new(director_params)
 
+    if @director.save
+      redirect_to root_path, notice: 'Diretor cadastrado com sucesso!'
+    else
+      flash.now.notice = 'Não foi possível cadastrar diretor!'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -21,5 +30,15 @@ class DirectorsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def director_params
+    params.require(:director).permit(:name, :nationality, :date_of_birth, :genre_id)
+  end
+
+  def set_director
+    @director = Director.find(params[:id])
   end
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,6 @@
 class Director < ApplicationRecord
   belongs_to :genre
   has_many :movies
+
+  validates :name, :nationality, :date_of_birth, :genre_id, presence: true
 end
