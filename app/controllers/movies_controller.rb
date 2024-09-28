fe179c319b07650e17be6d707f68ee48686ce296
vando <vandorramos@gmail.com>
2024-09-28 14:58:34 -0300

    Creating a form for register new movies, config movies controller and model, and show them in home page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,23 @@
 class MoviesController < ApplicationController
+  before_action :set_director, only: %i[show edit update destroy]
+
   def show
 
   end
 
   def new
-
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.new(movie_params)
 
+    if @movie.save
+      redirect_to root_path, notice: 'Filme cadastrado com sucesso!'
+    else
+      flash.now.notice = 'Não foi possível cadastrar filme!'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,8 +27,18 @@ class MoviesController < ApplicationController
   def update
 
   end
-  
+
   def destroy
 
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_year, :synopsis, :country_of_origin, :duration, :director_id, :genre_id)
+  end
+
+  def set_movie
+    @director = Director.find(params[:id])
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :director
   belongs_to :genre
+
+  validates :title, :release_year, :synopsis, :country_of_origin, :duration, :director_id, :genre_id, presence: true
 end
